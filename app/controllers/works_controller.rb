a5d253ee723813c11d902674d1e9679242a3929a
Elise Pham <elise.pham88@gmail.com>
2019-04-22 16:44:17 -0700

    added header for each page, get work index page to show all work by categories

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,8 +1,13 @@
 class WorksController < ApplicationController
   def index
+    @works = Work.all
   end
 
   def show
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+    end
   end
 
   def new
@@ -18,5 +23,11 @@ class WorksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:title, :category, :publication_year, :creator)
   end
 end
