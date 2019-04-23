894c274bd7cfc57d063246553f319fd9806214da
Elise Pham <elise.pham88@gmail.com>
2019-04-22 20:51:19 -0700

    added tests for work validation

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,30 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { works(:avenger) }
 
   it "must be valid" do
-    value(work).must_be :valid?
+    valid = work.valid?
+    expect(valid).must_equal true
+  end
+
+  describe "validations" do
+    it "requires a title" do
+      work.title = nil
+
+      valid_work = work.valid?
+
+      expect(valid_work).must_equal false
+      expect(work.errors.messages).must_include :title
+      expect(work.errors.messages[:title]).must_equal ["can't be blank"]
+    end
+
+    it "requires a unique title" do
+      duplicate_work = Work.new(title: work.title)
+
+      expect(duplicate_work.save).must_equal false
+      expect(duplicate_work.errors.messages).must_include :title
+      expect(duplicate_work.errors.messages[:title]).must_equal ["has already been taken"]
+    end
   end
 end
