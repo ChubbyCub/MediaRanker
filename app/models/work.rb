a8c8c95185a4e997d77d7ee735214e0297aadff4
Elise Pham <elise.pham88@gmail.com>
2019-04-23 22:42:14 -0700

    establish relationship between vote-user-work

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,7 @@
 class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :category, presence: true
+
+  has_many :votes
+  has_many :users, :through => :votes
 end
