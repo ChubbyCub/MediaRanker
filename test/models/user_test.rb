f825d062d392f0b01fbd4e67e17cf4f05b95f898
Elise Pham <elise.pham88@gmail.com>
2019-04-23 22:27:04 -0700

    added validation tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,30 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  let(:user) { users(:two) }
 
   it "must be valid" do
-    value(user).must_be :valid?
+    is_valid = user.valid?
+    value(is_valid).must_equal true
+  end
+
+  describe "validations" do
+    it "requires a username" do
+      user.username = nil
+
+      valid_user = user.valid?
+
+      expect(valid_user).must_equal false
+      expect(user.errors.messages).must_include :username
+      expect(user.errors.messages[:username]).must_equal ["can't be blank"]
+    end
+
+    it "requires a unique username" do
+      duplicate_user = User.new(username: user.username)
+
+      expect(duplicate_user.save).must_equal false
+      expect(duplicate_user.errors.messages).must_include :username
+      expect(duplicate_user.errors.messages[:username]).must_equal ["has already been taken"]
+    end
   end
 end
