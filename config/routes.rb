c5c37a231248bbece57546b4938fa1bf39e322fc
Elise Pham <elise.pham88@gmail.com>
2019-04-23 19:53:22 -0700

    completed user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: "homepages#index"
   resources :works
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
