8b6e38f431e2edabc6e9eb4c1848871e0209a09c
Elise Pham <elise.pham88@gmail.com>
2019-04-26 20:09:27 -0700

    fix the js script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,12 +8,13 @@
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.  //= require jquery3
-  //= require popper
-  //= require bootstrap-sprockets
+// about supported directives.  
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 
 //
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require_tree
